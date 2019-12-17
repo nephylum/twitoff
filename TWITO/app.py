53d5e5782e3ae0b6ec2dcf0dd664832c96c46e1a
nephylum <25045705+nephylum@users.noreply.github.com>
2019-12-17 15:16:36 -0800

    fixed app.py

--- TWITO/app.py
+++ TWITO/app.py
@@ -1,14 +1,14 @@
 """code for the app"""
-
+from decouple import config
 from flask import Flask
-from.models import DB
+from .models import DB, User
 #make an app factory
 
 def create_app():
     app = Flask(__name__)
 
     #add config for the database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\eneph\\twitoff\\TWITO\\db.sqlite3'
     #have the database know the about the app
     DB.init_app(app)
 
