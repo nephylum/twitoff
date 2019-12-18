2ffcc0d86be97b9fbf428bc511e214310e8cc301
nephylum <25045705+nephylum@users.noreply.github.com>
2019-12-18 06:52:24 -0800

    changing schema

--- TWITO/models.py
+++ TWITO/models.py
@@ -8,6 +8,9 @@ class User(DB.Model):
     """Twitter users for analysis"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 class Tweet(DB.Model):
     """Tweets pulled"""
@@ -15,3 +18,5 @@ class Tweet(DB.Model):
     text= DB.Column(DB.Unicode(300))
     uid = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
